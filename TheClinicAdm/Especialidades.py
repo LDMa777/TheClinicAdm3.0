b8b854ae10a1a671469b397a477da2b9e469bd0c
ExZero777 <ldmarques007@gmail.com>
2022-01-06 15:21:36 -0300

    se crea clase Especialides

---
+++ TheClinicAdm/Especialidades.py
@@ -0,0 +1,24 @@
+class Especialidades:
+    def __init__(self, id_especialidad, nombre_especialidad):
+        self._id_especialidad = id_especialidad
+        self._nombre_especialidad = nombre_especialidad
+
+    def __str__(self):
+        return f'[ID:{self._id_especialidad},Nombre:{self._nombre_especialidad}]'
+
+    @property
+    def id_especialidad(self):
+        return self._id_especialidad
+
+    @id_especialidad.setter:
+    def id_especialidad(self, id_especialidad):
+        self._id_especialidad = id_especialidad
+
+    @property
+    def nombre_especialidad(self):
+        return self._nombre_especialidad
+
+    @id_especialidad.setter
+    def nombre_especialidad(self, nombre_especialidad):
+        self._nombre_especialidad = nombre_especialidad
+
